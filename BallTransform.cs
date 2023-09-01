ad6b5a3c3ca5b1f7e3cf7808afad9ccabba9967e
Akhilesh Moorthy <akhileshmoorthy2010@gmail.com>
2023-09-01 10:49:03 -0700

    Update BallTransform.cs

--- BallTransform.cs
+++ BallTransform.cs
@@ -14,6 +14,6 @@ public class BallTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += scaleChange;
+        transform.localScale += scaleChange;
     }
 }
